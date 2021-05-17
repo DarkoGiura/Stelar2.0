c8e6431fc55fe8d416b807c5d6504378f03317d9
DarkoGiura <darko.giura00@e-uvt.ro>
2021-05-17 20:35:38 +0300

    Stelar

---
+++ app/src/main/java/com/example/stelar20/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,43 @@
+package com.example.stelar20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.support.v4.content.ContextCompat.startActivity;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+    private Button mLogin, mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ChooseLoginRegistrationActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/stelar20/arrayAdapter.java
@@ -0,0 +1,4 @@
+package com.example.stelar20;
+
+public enum arrayAdapter {
+}
